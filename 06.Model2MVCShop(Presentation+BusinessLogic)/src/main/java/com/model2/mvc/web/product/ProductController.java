1bc00020400e1b6df2417e69405e15b7ba567d6d
USER <USER@USER-PC>
2019-04-30 13:49:47 +0900

    new/div: ProductController 구현완료

--- 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/product/ProductController.java
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/product/ProductController.java
@@ -1,5 +1,9 @@
 package com.model2.mvc.web.product;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,8 +13,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.model2.mvc.common.Page;
+import com.model2.mvc.common.Search;
 import com.model2.mvc.service.domain.Product;
-import com.model2.mvc.service.domain.User;
 import com.model2.mvc.service.product.ProductService;
 
 
@@ -63,4 +68,67 @@ public class ProductController {
 			return "forward:/product/readProduct.jsp";
 		}
 		
-}
+		@RequestMapping("/updateProductView.do")
+		public String updateProductView( @RequestParam("prodNo") int prodNo , Model model ) throws Exception{
+
+			System.out.println("/updateProductView.do");
+			
+			Product product = productService.getProduct(prodNo);
+			
+			model.addAttribute("product", product);
+			
+			return "forward:/product/updateProduct.jsp";
+		}
+		
+		@RequestMapping("/updateProduct.do")
+		public String updateProduct( @ModelAttribute("product") Product product , Model model ) throws Exception{
+
+			System.out.println("/updateProduct.do");
+
+			productService.updateProduct(product);
+			
+			return "redirect:/getProduct.do?prodNo="+product.getProdNo();
+		}
+		
+		@RequestMapping("/listProduct.do")
+		public String listProduct( @ModelAttribute("search") Search search ,
+													Model model, HttpServletRequest request) throws Exception{
+			
+			System.out.println("/listProduct.do");
+			
+			if(request.getParameter("menu").equals("cartList")) {
+				search.setCart(true);
+			}else {
+				search.setCart(false);
+			}
+			
+			if(search.getCurrentPage() ==0 ){
+				search.setCurrentPage(1);
+			}
+			
+			if(request.getParameter("currentPage") != null && !request.getParameter("currentPage").equals("")) {
+				System.out.println("들어온 currentPage 값 :: "+request.getParameter("currentPage"));
+				search.setCurrentPage(Integer.parseInt(request.getParameter("currentPage")));
+			}
+			
+			if(request.getParameter("pageCondition") != null && !request.getParameter("pageCondition").equals("")) {
+				pageSize = Integer.parseInt(request.getParameter("pageCondition"));
+			}
+			
+			search.setPageSize(pageSize);
+			
+			// Business logic 수행
+			Map<String , Object> map=productService.getProductList(search);
+			
+			Page resultPage = new Page( search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
+			System.out.println(resultPage);
+			
+			// Model 과 View 연결
+			model.addAttribute("list", map.get("list"));
+			model.addAttribute("resultPage", resultPage);
+			model.addAttribute("search", search);
+			
+			return "forward:/product/listProduct.jsp";
+		}
+		
+}//end of class
